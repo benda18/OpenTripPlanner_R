34b29f46cd886514adfb58b93f4077dd44a4a392
Tim Bender <bendertj@gmail.com>
2025-10-22 19:22:41 -0400

    jar download

--- R/explore.R
+++ R/explore.R
@@ -1,2 +1,12 @@
 library(renv)
 library(opentripplanner)
+
+snapshot()
+
+
+xdemo <- opentripplanner::otp_dl_demo(path_data = "C:/Users/bende/Documents/R/play/OpenTripPlanner_R/demo_otp")
+
+?opentripplanner::otp_dl_demo()
+
+
+getwd()
