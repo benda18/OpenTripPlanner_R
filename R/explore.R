c021dcc49e6a69086da36493c9a031070aee1b7c
Tim Bender <bendertj@gmail.com>
2025-11-22 12:07:08 -0500

    periodic

--- R/explore.R
+++ R/explore.R
@@ -4,9 +4,11 @@ library(opentripplanner)
 snapshot()
 
 
-xdemo <- opentripplanner::otp_dl_demo(path_data = "C:/Users/bende/Documents/R/play/OpenTripPlanner_R/demo_otp")
+xdemo <- otp_dl_demo(path_data = "C:/Users/bende/Documents/R/play/OpenTripPlanner_R/demo_otp")
 
-?opentripplanner::otp_dl_demo()
+log1 <- opentripplanner::otp_build_graph(otp = "C:/Users/bende/AppData/Local/R/win-library/4.5/opentripplanner/jar/otp-1.5.0-shaded.jar", 
+                                         dir = "C:/Users/bende/Documents/R/play/OpenTripPlanner_R/demo_otp", 
+                                         otp_version = 1.5)
+opentripplanner::otp_dl_jar()
 
-
-getwd()
+# next step: download java
